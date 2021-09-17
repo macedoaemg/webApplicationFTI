6cc968e4652e7e4835f52c620f985662da23e653
anderson macedo <macedo.anderson@gmail.com>
2021-09-17 10:06:49 -0300

    verbos

---
+++ webApplicationFTI/webApplicationFTI/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApplicationFTI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("soma/{valor1}/{valor2}")]
+        public IActionResult Getsomar(string valor1, string valor2)
+        {
+            
+
+            return BadRequest("Valor inválido");
+        }
+                
+    }
+}
